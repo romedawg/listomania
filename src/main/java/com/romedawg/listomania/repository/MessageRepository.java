f8de2faff93fb2f078b4e3be1a8ee44962784ac4
Roman Rafacz <romanr32@gmail.com>
2024-09-26 16:33:56 -0500

    Fixed phonenumber/user lookup and message validation

--- src/main/java/com/romedawg/listomania/repository/MessageRepository.java
+++ src/main/java/com/romedawg/listomania/repository/MessageRepository.java
@@ -4,9 +4,11 @@ import com.romedawg.listomania.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @Query("SELECT DISTINCT m.data FROM Message m WHERE m.category=(:category)")
--- src/main/java/com/romedawg/listomania/repository/PersonRepository.java
+++ src/main/java/com/romedawg/listomania/repository/PersonRepository.java
@@ -4,18 +4,19 @@ import com.romedawg.listomania.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     @Query("SELECT DISTINCT p.id FROM Person p WHERE p.phoneNumber=(:phoneNumber)")
     Integer findPersonByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 
     @Query("SELECT DISTINCT p.id FROM Person p WHERE p.id=(:user_id)")
-    Integer findPersonById(@Param("user_id") Integer user_id);
+    List<Person> findPersonById(@Param("user_id") Integer user_id);
 
     @Query("SELECT p FROM Person p WHERE p.phoneNumber=(:phoneNumber)")
     List<Person> findPerson(@Param("phoneNumber") String phoneNumber);
-
 }
